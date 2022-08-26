14626872a72951f55a298d966483f69de91f461b
kill3rstabs <nashitnoorali78@gmail.com>
2022-08-26 19:19:53 +0000

    modified

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 from flask import * 
-import object_tracker
+import argparse
+import time
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+import detect
 app = Flask(__name__)
 @app.route('/')
 def helloWorld():
@@ -14,7 +29,7 @@ def success():
         # sys.argv = []
         f.save('static/'+f.filename)
         
-        object_tracker.main('./static/'+f.filename)
+        detect.detect('./static/'+f.filename)
         return redirect("/endpoint")
         
 @app.route('/endpoint',methods= ['GET','POST'])
